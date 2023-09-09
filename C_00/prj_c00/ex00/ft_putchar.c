38882ab865b4e7b6714e6a1ca89ff0c2dca85019
Hilal <46250773+hilalsaygin@users.noreply.github.com>
2023-09-09 21:18:39 +0300

    Delete C_00/prj_c00 directory

--- C_00/prj_c00/ex00/ft_putchar.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-//prints out only one character on console
-void ft_putchar(char c){
-
-    
-    write(1,&c,1);
-}
--- C_00/prj_c00/ex01/ft_print_alphabet.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-void ft_print_alphabet(void){
-    char ch = 'a';
-    while(ch <= 'z'){
-        write(1,&ch,1);
-        ch++;
-    }
-}
