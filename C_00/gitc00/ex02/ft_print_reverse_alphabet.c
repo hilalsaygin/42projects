0a1b866942390895f564cf8002d9451380183954
Hilal <46250773+hilalsaygin@users.noreply.github.com>
2023-09-15 19:45:51 +0300

    Update ft_print_reverse_alphabet.c

--- C_00/gitc00/ex02/ft_print_reverse_alphabet.c
+++ C_00/gitc00/ex02/ft_print_reverse_alphabet.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_print_reverse_alphabet(void)
 {
 	char	ch;
