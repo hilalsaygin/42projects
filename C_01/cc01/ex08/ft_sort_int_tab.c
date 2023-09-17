cfff29ec856ac168e60c505b75038a34e98db1f5
Hilal <46250773+hilalsaygin@users.noreply.github.com>
2023-09-18 00:26:31 +0300

    add norm checked c01

--- C_01/cc01/ex08/ft_sort_int_tab.c
+++ C_01/cc01/ex08/ft_sort_int_tab.c
@@ -6,12 +6,9 @@
 /*   By: hilal <hilal@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 23:40:54 by hilal             #+#    #+#             */
-/*   Updated: 2023/09/15 20:47:09 by hilal            ###   ########.fr       */
+/*   Updated: 2023/09/18 00:21:26 by hilal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-#include <stdio.h>
 
 void	ft_sort_int_tab(int *tab, int size)
 {
@@ -32,14 +29,4 @@ void	ft_sort_int_tab(int *tab, int size)
 		tab[j + 1] = current;
 		i++;
 	}
-}
-
-int main ( ){
-    int ll[10] = {23,4,55,6,1,203};
-
-    ft_sort_int_tab(ll, 6);
-    for (int i = 0; i<6;i++ )
-    {
-        printf ("%d**", ll[i]);
-    }
 }
